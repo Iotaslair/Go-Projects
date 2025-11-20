69818ccf4ba18586d946d79080237347b2d13b8a
Iotaslair <billypembleton1@gmail.com>
2025-11-19 22:15:34 -0500

    Finished with integer test

---
+++ learn_go_with_tests/integers/adder_test.go
@@ -0,0 +1,20 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	got := Add(2, 3)
+	want := 5
+	if got != want {
+		t.Errorf("Got %q want %q", got, want)
+	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
